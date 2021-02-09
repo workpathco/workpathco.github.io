aa8ced56ee007cb33f8cbf44258e693eb4c9e075
thetechwitch <ashley.wilson.freelance@gmail.com>
2021-02-09 11:06:32 -0500

    feat(index): added code block styling, google tag manager, search bar, example sidenav styling code that will be merged with the existing sidenav css

---
+++ app.js
@@ -0,0 +1,16 @@
+function search() {
+  var name = document.getElementById("searchForm").elements["searchItem"].value;
+  var pattern = name.toLowerCase();
+  var targetId = "";
+
+  var divs = document.getElementsByClassName("col-6");
+  for (var i = 0; i < divs.length; i++) {
+    var para = divs[i].getElementsByTagName("p");
+    var index = para[0].innerText.toLowerCase().indexOf(pattern);
+    if (index != -1) {
+      targetId = divs[i].parentNode.id;
+      document.getElementById(targetId).scrollIntoView();
+      break;
+    }
+  }
+}
