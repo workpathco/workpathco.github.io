07da441d82db4319732e807508acf9a2ec1f7c9e
thetechwitch <ashley.wilson.freelance@gmail.com>
2021-02-09 14:56:14 -0500

    feat(sidenav): working better now, styling makes more sense, figuring out best place for interior anchor points

--- js/sidebar-accordion.js
+++ js/sidebar-accordion.js
@@ -1,19 +1,17 @@
-$(document).ready(function() {
-	$('.menu li:has(ul)').click(function(e) {
-		e.preventDefault();
+$(document).ready(function () {
+  $(".menu li:has(ul)").click(function (e) {
+    if ($(this).hasClass("activado")) {
+      $(this).removeClass("activado");
+      $(this).children("ul").slideUp();
+    } else {
+      $(".menu li ul").slideUp();
+      $(".menu li").removeClass("activado");
+      $(this).addClass("activado");
+      $(this).children("ul").slideDown();
+    }
 
-		if($(this).hasClass('activado')) {
-			$(this).removeClass('activado');
-			$(this).children('ul').slideUp();
-		} else {
-			$('.menu li ul').slideUp();
-			$('.menu li').removeClass('activado');
-			$(this).addClass('activado');
-			$(this).children('ul').slideDown();
-		}
-
-		$('.menu li ul li a').click(function() {
-			window.location.href = $(this).attr('href');
-		})
-	});
+    $(".menu li ul li a").click(function () {
+      window.location.href = $(this).attr("href");
+    });
+  });
 });
