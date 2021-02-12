cc3cba3e55d2524168bfcc7278705a294973a9fe
thetechwitch <ashley.wilson.freelance@gmail.com>
2021-02-12 16:53:07 -0500

    feat(darkmode): playing with settings for light mode/dark mode

--- js/app.js
+++ js/app.js
@@ -1,3 +1,59 @@
+//Initialize code block formatter
+hljs.initHighlightingOnLoad();
+
+//Light/Dark Mode initializer
+document.addEventListener("DOMContentLoaded", function (event) {
+  document.documentElement.setAttribute("data-theme", "light");
+  var switchButtons = document.getElementById("themeButton");
+  var switchLogos = document.getElementById("themeLogo");
+  var themeSwitcher = document.getElementById("theme-switcher");
+  themeSwitcher.onclick = function () {
+    var currentTheme = document.documentElement.getAttribute("data-theme");
+    var switchToTheme = currentTheme === "dark" ? "light" : "dark";
+    document.documentElement.setAttribute("data-theme", switchToTheme);
+    if (switchToTheme === "dark") {
+      switchButtons.innerHTML =
+        "<span class='badge rounded-pill bg-warning' id='theme-switcher' onclick='lightDarkMode();'><i class='bi bi-brightness-high-fill'></i></span>";
+      switchLogos.innerHTML =
+        "<a href='#Top'><img src='images/workpath_logo_dark.png' alt='Workpath API' width='180px' style='padding-bottom: 10px' /></a>";
+    } else {
+      switchButtons.innerHTML =
+        "<span class='badge rounded-pill bg-dark' id='theme-switcher' onclick='lightDarkMode();'><i class='bi bi-moon'></i></span>";
+      switchLogos.innerHTML =
+        "<a href='#Top'><img src='images/workpath_logo.png' alt='Workpath API' width='180px' style='padding-bottom: 10px' /></a>";
+    }
+  };
+});
+
+//Light/Dark Mode after initial button click
+function lightDarkMode() {
+  var switchLogos = document.getElementById("themeLogo");
+  var switchButtons = document.getElementById("themeButton");
+  var currentTheme = document.documentElement.getAttribute("data-theme");
+  var switchToTheme = currentTheme === "dark" ? "light" : "dark";
+  document.documentElement.setAttribute("data-theme", switchToTheme);
+  if (switchToTheme === "dark") {
+    switchButtons.innerHTML =
+      "<span class='badge rounded-pill bg-warning' id='theme-switcher' onclick='lightDarkMode();'><i class='bi bi-brightness-high-fill'></i></span>";
+    switchLogos.innerHTML =
+      "<a href='#Top'><img src='images/workpath_logo_dark.png' alt='Workpath API' width='180px' style='padding-bottom: 10px' /></a>";
+  } else {
+    switchButtons.innerHTML =
+      "<span class='badge rounded-pill bg-dark' id='theme-switcher' onclick='lightDarkMode();'><i class='bi bi-moon'></i></span>";
+    switchLogos.innerHTML = "<a href='#Top'><img src='images/workpath_logo.png' alt='Workpath API' width='180px' style='padding-bottom: 10px' /></a>";
+  }
+}
+
+//Accordian Sidenav
+function changeClassName(nav) {
+  var newNavTab = document.getElementById(nav);
+  if (newNavTab.className === "hidden") {
+    newNavTab.className = "selected";
+  } else {
+    newNavTab.className = "hidden";
+  }
+}
+
 // function search() {
 //   var name = document.getElementById("searchForm").elements["searchItem"].value;
 //   var pattern = name.toLowerCase();
@@ -14,12 +70,3 @@
 //     }
 //   }
 // }
-
-function changeClassName(nav) {
-  var newNavTab = document.getElementById(nav);
-  if (newNavTab.className === "hidden") {
-    newNavTab.className = "selected";
-  } else {
-    newNavTab.className = "hidden";
-  }
-}
